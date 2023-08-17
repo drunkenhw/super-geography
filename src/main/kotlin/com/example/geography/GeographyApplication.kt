e99d900be6e0f0fcc8f57a91fc745df56fcd542d
drunkenhw <drunkenhw@gmail.com>
2023-08-17 21:22:46 +0900

    init

---
+++ src/main/kotlin/com/example/geography/GeographyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.geography
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeographyApplication
+
+fun main(args: Array<String>) {
+    runApplication<GeographyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/geography/GeographyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.geography
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeographyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
